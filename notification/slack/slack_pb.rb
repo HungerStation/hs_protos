93632e1bdbddebc36b567dcf4a838bea789c8407
Kiki Luqman Hakiem <kiki.hakiem@gmail.com>
2020-12-10 14:36:57 +0700

    feat: support sending slack message with attachment

--- notification/slack/slack_pb.rb
+++ notification/slack/slack_pb.rb
@@ -12,8 +12,26 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
       optional :source, :string, 3
       optional :webhook, :string, 4
       optional :level, :enum, 5, "protos.notification.slack.Request.SeverityLevel"
-      map :payload, :string, :string, 6
+      optional :payload, :message, 6, "protos.notification.slack.Request.Message"
       optional :event_timestamp, :message, 7, "google.protobuf.Timestamp"
+    end
+    add_message "protos.notification.slack.Request.Field" do
+      optional :title, :string, 1
+      optional :value, :string, 2
+      optional :short, :bool, 3
+    end
+    add_message "protos.notification.slack.Request.Attachment" do
+      optional :color, :string, 1
+      optional :fallback, :string, 2
+      optional :title, :string, 3
+      optional :title_link, :string, 4
+      optional :pretext, :string, 5
+      optional :text, :string, 6
+      repeated :fields, :message, 7, "protos.notification.slack.Request.Field"
+    end
+    add_message "protos.notification.slack.Request.Message" do
+      optional :text, :string, 1
+      repeated :attachments, :message, 2, "protos.notification.slack.Request.Attachment"
     end
     add_enum "protos.notification.slack.Request.SeverityLevel" do
       value :INFO, 0
@@ -30,6 +48,9 @@ module Protos
   module Notification
     module Slack
       Request = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("protos.notification.slack.Request").msgclass
+      Request::Field = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("protos.notification.slack.Request.Field").msgclass
+      Request::Attachment = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("protos.notification.slack.Request.Attachment").msgclass
+      Request::Message = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("protos.notification.slack.Request.Message").msgclass
       Request::SeverityLevel = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("protos.notification.slack.Request.SeverityLevel").enummodule
       Response = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("protos.notification.slack.Response").msgclass
     end
