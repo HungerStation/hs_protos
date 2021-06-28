66959c9d5d24a383676610333f6412d6f7dce96f
Rudy Zidan <rz.zidan@gmail.com>
2021-06-28 17:31:13 +0300

    chore: update proto

--- vendor-search/restaurant/restaurant_pb.rb
+++ vendor-search/restaurant/restaurant_pb.rb
@@ -43,10 +43,8 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
       optional :android_cover_photo_url, :string, 34
       optional :enabled_reason, :int32, 35
       optional :restaurant_type, :int32, 36
-      optional :rate_average, :float, 37
-      optional :rate_count, :int32, 38
       optional :vertical_id, :int32, 39
-      optional :boolean, :bool, 40
+      optional :disable_order_note, :bool, 40
     end
     add_message "Restaurant.Metadata" do
       optional :guid, :string, 1
